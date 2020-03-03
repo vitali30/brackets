36e4a2a9170f20a2939a5f3a473bb77522e5e281
vitali30 <vysotskivitali@gmail.com>
2020-03-03 15:39:03 +0300

    add file index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ module.exports = function check(str, bracketsConfig) {
   
   const brackets = bracketsConfig.map(function entering(element){return element.join('');});
 
-  // console.log(brackets);
-
-  //console.log(str)
   for(let i =0 ; i< str.length + 50 ; i++){
   brackets.forEach(function deleting(element){str = str.replace(element, '')});
   
   }
-  // console.log(str)
-  //console.log(!str)
+  
   return (!str)
 }
-module.exports('((()))()',[['(', ')']])
- // -> true
-//  assert.equal(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7), false);
-//  [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]
+
 
